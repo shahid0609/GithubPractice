a841008f575c374e06b9c34a091501bc17e55d3f
shahid0609 <shahidpathan061@gmail.com>
2024-11-27 12:58:58 +0530

    shahid

---
+++ src/Sample1/Demo1.java
@@ -0,0 +1,48 @@
+package Sample1;
+
+public class Demo1 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		System.out.println("shahid");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
