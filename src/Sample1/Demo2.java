e6411b9c756f279fd021e9ab0aa1e6cfab4beb28
shahid0609 <shahidpathan061@gmail.com>
2024-11-28 12:11:33 +0530

    demo2 file added

---
+++ src/Sample1/Demo2.java
@@ -0,0 +1,44 @@
+package Sample1;
+
+public class Demo2 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		
+		
+		System.out.println("running Demo2 class ");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
