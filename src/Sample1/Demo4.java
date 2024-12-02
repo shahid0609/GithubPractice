59fba2d427d15e354b975eb7a2849adb2ac7e754
shahid0609 <shahidpathan061@gmail.com>
2024-12-02 14:06:34 +0530

    added Demo4 file

---
+++ src/Sample1/Demo4.java
@@ -0,0 +1,24 @@
+package Sample1;
+
+public class Demo4 {
+
+	
+	
+	public static void main(String[] args) {
+		
+		
+		
+		System.out.println("Good Afternoon");
+		
+		
+		
+		
+			
+		
+	}
+	
+	
+	
+		
+	
+}
