9659f74e425a6c415b08463192fd9d2d94a9c2c4
shahid0609 <shahidpathan061@gmail.com>
2025-01-17 20:11:24 +0530

    Demo7

---
+++ src/Sample1/Demo7.java
@@ -0,0 +1,37 @@
+package Sample1;
+
+public class Demo7 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+	System.out.println("Tumsar");	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
