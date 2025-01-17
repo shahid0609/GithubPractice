e51a22a1102bfac1df0f15d45d4efd7e62579fc1
shahid0609 <shahidpathan061@gmail.com>
2025-01-17 19:46:33 +0530

    Demo5

---
+++ src/Sample1/Demo5.java
@@ -0,0 +1,33 @@
+package Sample1;
+
+public class Demo5 {
+
+	
+	public static void main(String [] args)
+	{
+		
+		
+		System.out.println("GOOD EVENING");
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
