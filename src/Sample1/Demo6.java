5c09ccbb585da2bd6ec1c4263d437b75c036a372
shahid0609 <shahidpathan061@gmail.com>
2025-01-17 20:07:25 +0530

    Demo6

---
+++ src/Sample1/Demo6.java
@@ -0,0 +1,34 @@
+package Sample1;
+
+public class Demo6 {
+
+	
+	public static void main(String[] args) {
+		
+		
+		
+		
+		System.out.println("Nagpur");
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
